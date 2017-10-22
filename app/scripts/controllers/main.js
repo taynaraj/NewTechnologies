39e4fd437e1b0e353a63346441002df7d32f101a
Taynara Jaeggger <taynaraj@gmail.com>
2017-10-22 16:04:56 -0200

    Primeiro commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name estudosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the estudosApp
+ */
+angular.module('estudosApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
